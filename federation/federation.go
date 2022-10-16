6d9c9987428924ce7f463bc765ec0238fd0d52ba
dariuszkuc <9501705+dariuszkuc@users.noreply.github.com>
2022-10-16 14:00:50 -0500

    initial version of federation support

---
+++ federation/federation.go
@@ -0,0 +1,207 @@
+package federation
+
+import (
+	"fmt"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/graphql/language/ast"
+)
+
+type FederatedSchemaConfig struct {
+	EntitiesFieldResolver graphql.FieldResolveFn
+	EntityTypeResolver    graphql.ResolveTypeFn
+	graphql.SchemaConfig
+}
+
+// federated types
+
+type _Any map[string]interface{}
+
+var _AnyType = graphql.NewScalar(graphql.ScalarConfig{
+	Name:        "_Any",
+	Description: "The `_Any` scalar is used to pass representations of entities from external services into the root _entities field for execution.",
+	Serialize: func(value interface{}) interface{} {
+		switch value := value.(type) {
+		case _Any:
+			return value
+		case *_Any:
+			return *value
+		default:
+			return nil
+		}
+	},
+	ParseValue: func(value interface{}) interface{} {
+		return value
+	},
+	ParseLiteral: func(valueAST ast.Value) interface{} {
+		switch valueAST := valueAST.(type) {
+		case *ast.ListValue:
+			return valueAST.Values
+		case *ast.BooleanValue:
+			return valueAST.Value
+		case *ast.EnumValue:
+			return valueAST.Value
+		case *ast.FloatValue:
+			return valueAST.Value
+		case *ast.IntValue:
+			return valueAST.Value
+		case *ast.ObjectValue:
+			literalValue := make(map[string]ast.Value)
+			for _, field := range valueAST.Fields {
+				literalValue[field.Name.Value] = field.Value
+			}
+			return literalValue
+		case *ast.StringValue:
+			return valueAST.Value
+		default:
+			return nil
+		}
+	},
+})
+
+func coerceString(value interface{}) interface{} {
+	if v, ok := value.(*string); ok {
+		if v == nil {
+			return nil
+		}
+		return *v
+	}
+	return fmt.Sprintf("%v", value)
+}
+
+var _FieldSetType = graphql.NewScalar(graphql.ScalarConfig{
+	Name:        "FieldSet",
+	Description: "String-serialized scalar represents a set of fields that's passed to a federated directive, such as @key, @requires, or @provides",
+	// coercing logic is the same as String
+	Serialize: func(value interface{}) interface{} {
+		return coerceString(value)
+	},
+	ParseValue: func(value interface{}) interface{} {
+		return coerceString(value)
+	},
+	ParseLiteral: func(valueAST ast.Value) interface{} {
+		switch valueAST := valueAST.(type) {
+		case *ast.StringValue:
+			return valueAST.Value
+		}
+		return nil
+	},
+})
+
+type _Service struct {
+	SDL string `json:"sdl"`
+}
+
+var _ServiceType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "_Service",
+	Fields: graphql.Fields{
+		"sdl": &graphql.Field{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	},
+})
+
+func findEntityTypes(schema graphql.Schema) []*graphql.Object {
+	entities := make([]*graphql.Object, 0)
+
+	for _, gqlType := range schema.TypeMap() {
+		obj, ok := gqlType.(*graphql.Object)
+		if ok && isEntity(obj) {
+			entities = append(entities, obj)
+		}
+	}
+	return entities
+}
+
+func isEntity(t *graphql.Object) bool {
+	for _, directive := range t.AppliedDirectives {
+		if directive.Name == "key" {
+			return true
+		}
+	}
+	return false
+}
+
+// @link(import : ["composeDirective", "external", "inaccessible", "key", "override", "provides", "requires", "shareable", "tag", "FieldSet"], url : "https://specs.apollo.dev/federation/v2.1")
+var federationLinkAppliedDirective = LinkAppliedDirective(
+	"https://specs.apollo.dev/federation/v2.1",
+	[]string{"composeDirective", "external", "inaccessible", "key", "override", "provides", "requires", "shareable", "tag", "FieldSet"},
+)
+
+// new schema
+
+func NewFederatedSchema(config FederatedSchemaConfig) (graphql.Schema, error) {
+	// add federated directives
+	config.Directives = append(config.Directives,
+		// built-in directives
+		graphql.DeprecatedDirective,
+		graphql.IncludeDirective,
+		graphql.SkipDirective,
+		// federated directives
+		ComposeDirectiveDefinition,
+		ExternalDirectiveDefinition,
+		InaccessibleDirectiveDefinition,
+		KeyDirectiveDefinition,
+		LinkDirectiveDefinition,
+		OverrideDirectiveDefinition,
+		ProvidesDirectiveDefinition,
+		RequiresDirectiveDefinition,
+		ShareableDirectiveDefinition,
+		TagDirectiveDefinition,
+	)
+
+	// add @link directive to the schema
+	config.AppliedDirectives = append(config.AppliedDirectives, federationLinkAppliedDirective)
+
+	schema, ok := graphql.NewSchema(config.SchemaConfig)
+
+	// add federated types
+	// scalar _Any
+	schema.TypeMap()["_Any"] = _AnyType
+
+	// scalar FieldSet
+	schema.TypeMap()["FieldSet"] = _FieldSetType
+
+	// type _Service
+	schema.AppendType(_ServiceType)
+	schema.QueryType().AddFieldConfig("_service", &graphql.Field{
+		Name: "_service",
+		Type: _ServiceType,
+	})
+
+	// union _Entity
+	entities := findEntityTypes(schema)
+	if len(entities) > 0 {
+		entityType := graphql.NewUnion(
+			graphql.UnionConfig{
+				Name:        "_Entity",
+				Types:       entities,
+				ResolveType: config.EntityTypeResolver,
+			},
+		)
+
+		schema.TypeMap()["_Entity"] = entityType
+		schema.QueryType().AddFieldConfig("_entities", &graphql.Field{
+			Name: "_entities",
+			Type: graphql.NewList(entityType),
+			Args: graphql.FieldConfigArgument{
+				"representations": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.NewList(graphql.NewNonNull(_AnyType))),
+				},
+			},
+			Resolve: config.EntitiesFieldResolver,
+		})
+	}
+
+	// update _service { sdl } resolver to correct value
+	sdl, ok := PrintSchema(schema)
+	println(sdl)
+	schema.QueryType().AddFieldConfig("_service", &graphql.Field{
+		Name: "_service",
+		Type: _ServiceType,
+		Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+			return &_Service{SDL: sdl}, nil
+		},
+	})
+	return schema, ok
+}
---
+++ federation/schema_printer.go
@@ -0,0 +1,385 @@
+package federation
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/graphql-go/graphql"
+)
+
+func printDescription(desc string, indent int, out *strings.Builder) {
+	if desc == "" {
+		return
+	}
+
+	if indent > 0 {
+		out.WriteString(strings.Repeat(" ", indent))
+	}
+	maxLineLength := 80 - indent - 4
+
+	if !strings.Contains(desc, "\"") && len(desc) < maxLineLength {
+		out.WriteString("\"")
+		out.WriteString(desc)
+		out.WriteString("\"\n")
+	} else {
+		out.WriteString("\"\"\"\n")
+		out.WriteString(strings.Repeat(" ", indent))
+		out.WriteString(desc)
+		out.WriteString("\n")
+		out.WriteString(strings.Repeat(" ", indent))
+		out.WriteString("\"\"\"\n")
+	}
+}
+
+// schema
+
+func printSchemaDefinition(schema graphql.Schema, out *strings.Builder) error {
+	out.WriteString("schema ")
+	printAppliedDirectives(schema.AppliedDirectives(), out)
+
+	if schema.QueryType() != nil {
+		out.WriteString("{\n")
+		fmt.Fprintf(out, "  query: %v\n", schema.QueryType().Name())
+	} else {
+		panic("invalid schema - schema requires valid query type")
+	}
+
+	if schema.MutationType() != nil {
+		fmt.Fprintf(out, "  mutation: %v\n", schema.MutationType().Name())
+	}
+	out.WriteString("}\n\n")
+	return nil
+}
+
+// directives
+
+func printDirectiveDefinitions(directives []*graphql.Directive, out *strings.Builder) {
+	for _, directive := range sortDirectiveDefinitions(directives) {
+		printDirectiveDefinition(directive, out)
+	}
+}
+
+func sortDirectiveDefinitions(directives []*graphql.Directive) []*graphql.Directive {
+	sorted := make([]*graphql.Directive, 0, len(directives))
+	for _, v := range directives {
+		sorted = append(sorted, v)
+	}
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i].Name < sorted[j].Name
+	})
+
+	return sorted
+}
+
+// Example
+// "Marks an element of a GraphQL schema as no longer supported."
+// directive @deprecated(reason: String) on FIELD_DEFINITION | ENUM_VALUE
+func printDirectiveDefinition(directive *graphql.Directive, out *strings.Builder) {
+	printDescription(directive.Description, 0, out)
+	out.WriteString("directive @")
+	out.WriteString(directive.Name)
+	if len(directive.Args) > 0 {
+		out.WriteString("(")
+
+		args := make([]string, 0, len(directive.Args))
+		for _, arg := range directive.Args {
+			switch arg.Type.(type) {
+			case *graphql.List:
+				args = append(args, fmt.Sprintf("%s: [%s]", arg.Name(), arg.Type.Name()))
+			default:
+				args = append(args, fmt.Sprintf("%s: %s", arg.Name(), arg.Type.Name()))
+			}
+		}
+		out.WriteString(strings.Join(args, ", "))
+		out.WriteString(")")
+	}
+
+	if directive.Repeatable {
+		out.WriteString(" repeatable")
+	}
+
+	out.WriteString(" on ")
+	out.WriteString(strings.Join(directive.Locations, " | "))
+	out.WriteString("\n\n")
+}
+
+func printAppliedDirectives(appliedDirectives []*graphql.AppliedDirective, out *strings.Builder) {
+	if len(appliedDirectives) == 0 {
+		return
+	}
+
+	for _, appliedDirective := range sortAppliedDirectives(appliedDirectives) {
+		out.WriteString(" ")
+		printAppliedDirective(appliedDirective, out)
+	}
+}
+
+func sortAppliedDirectives(appliedDirectives []*graphql.AppliedDirective) []*graphql.AppliedDirective {
+	sorted := make([]*graphql.AppliedDirective, 0, len(appliedDirectives))
+	for _, v := range appliedDirectives {
+		sorted = append(sorted, v)
+	}
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i].Name < sorted[j].Name
+	})
+
+	return sorted
+}
+
+func printAppliedDirective(applied *graphql.AppliedDirective, out *strings.Builder) {
+	fmt.Fprintf(out, "@%s", applied.Name)
+	if len(applied.Args) > 0 {
+		out.WriteString("(")
+
+		args := []string{}
+		for _, arg := range applied.Args {
+			value := printAppliedDirectiveArgumentValue(arg.Value)
+			if len(value) > 1 {
+				args = append(args, fmt.Sprintf("%s: [%s]", arg.Name, strings.Join(value, ", ")))
+			} else {
+				args = append(args, fmt.Sprintf("%s: %v", arg.Name, value[0]))
+			}
+		}
+		out.WriteString(strings.Join(args, ", "))
+		out.WriteString(")")
+	}
+}
+
+func printAppliedDirectiveArgumentValue(arg interface{}) []string {
+	printedValues := []string{}
+
+	switch reflect.TypeOf(arg).Kind() {
+	case reflect.Array, reflect.Slice:
+		array := reflect.ValueOf(arg)
+		for i := 0; i < array.Len(); i++ {
+			values := printAppliedDirectiveArgumentValue(array.Index(i).Interface())
+			printedValues = append(printedValues, values...)
+		}
+	case reflect.String:
+		printedValues = append(printedValues, fmt.Sprintf("%q", arg))
+	default:
+		printedValues = append(printedValues, fmt.Sprintf("%v", arg))
+	}
+	return printedValues
+}
+
+// enums
+
+func printEnumDefinitions(enums []*graphql.Enum, out *strings.Builder) {
+	sort.Slice(enums, func(i, j int) bool {
+		return enums[i].Name() < enums[j].Name()
+	})
+
+	for _, enum := range enums {
+		printDescription(enum.Description(), 0, out)
+		fmt.Fprintf(out, "enum %s", enum.Name())
+		printAppliedDirectives(enum.AppliedDirectives, out)
+		out.WriteString(" {\n")
+
+		// enum values
+		for _, enumValue := range enum.Values() {
+			printDescription(enumValue.Description, 2, out)
+			out.WriteString(enumValue.Name)
+			printAppliedDirectives(enum.AppliedDirectives, out)
+			out.WriteString("\n")
+		}
+
+		out.WriteString("}\n\n")
+	}
+}
+
+// input object
+
+func printInputObjectDefinitions(inputObjects []*graphql.InputObject, out *strings.Builder) {
+	sort.Slice(inputObjects, func(i, j int) bool {
+		return inputObjects[i].Name() < inputObjects[j].Name()
+	})
+
+	for _, inputObject := range inputObjects {
+		printDescription(inputObject.Description(), 0, out)
+		fmt.Fprintf(out, "input %s", inputObject.Name())
+		printAppliedDirectives(inputObject.AppliedDirectives, out)
+		out.WriteString(" {\n")
+		printInputObjectFieldDefinitions(inputObject.Fields(), out)
+		out.WriteString("}\n\n")
+	}
+}
+
+// input object fields
+
+func printInputObjectFieldDefinitions(inputFields graphql.InputObjectFieldMap, out *strings.Builder) {
+	keys := make([]string, 0, len(inputFields))
+	for k := range inputFields {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	// InputObjectFieldMap map[string]*InputObjectField
+	for _, key := range keys {
+		field := inputFields[key]
+		printDescription(field.Description(), 2, out)
+		fmt.Fprintf(out, "  %s: %s", field.Name(), field.Type.String())
+		printAppliedDirectives(field.AppliedDirectives, out)
+		out.WriteString("\n")
+	}
+}
+
+// interfaces
+
+func printInterfaceDefinitions(interfaces []*graphql.Interface, out *strings.Builder) {
+	sort.Slice(interfaces, func(i, j int) bool {
+		return interfaces[i].Name() < interfaces[j].Name()
+	})
+
+	for _, intf := range interfaces {
+		printDescription(intf.Description(), 0, out)
+		fmt.Fprintf(out, "interface %s", intf.Name())
+		printAppliedDirectives(intf.AppliedDirectives, out)
+		out.WriteString(" {\n")
+		printFieldDefinitions(intf.Fields(), out)
+		out.WriteString("}\n\n")
+	}
+}
+
+// objects
+
+func printObjectDefinitions(objects []*graphql.Object, out *strings.Builder) {
+	sort.Slice(objects, func(i, j int) bool {
+		return objects[i].Name() < objects[j].Name()
+	})
+
+	for _, object := range objects {
+		printDescription(object.Description(), 0, out)
+		fmt.Fprintf(out, "type %s", object.Name())
+		if len(object.Interfaces()) > 0 {
+			interfaces := make([]string, 0, len(object.Interfaces()))
+			for _, i := range object.Interfaces() {
+				interfaces = append(interfaces, i.Name())
+			}
+			out.WriteString(" implements")
+			out.WriteString(strings.Join(interfaces, ", "))
+		}
+		printAppliedDirectives(object.AppliedDirectives, out)
+		out.WriteString(" {\n")
+		printFieldDefinitions(object.Fields(), out)
+		out.WriteString("}\n\n")
+	}
+}
+
+func printFieldDefinitions(fieldDefinitionMap graphql.FieldDefinitionMap, out *strings.Builder) {
+	keys := make([]string, 0, len(fieldDefinitionMap))
+	for k := range fieldDefinitionMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		field := fieldDefinitionMap[key]
+		printDescription(field.Description, 2, out)
+		fmt.Fprintf(out, "  %s", field.Name)
+
+		if len(field.Args) > 0 {
+			out.WriteString("(")
+			args := make([]string, 0, len(field.Args))
+			for _, arg := range field.Args {
+				args = append(args, fmt.Sprintf("%s: %s", arg.Name(), arg.Type.Name()))
+				out.WriteString(strings.Join(args, ", "))
+			}
+			out.WriteString(")")
+		}
+
+		fmt.Fprintf(out, ": %s", field.Type.Name())
+		printAppliedDirectives(field.AppliedDirectives, out)
+		out.WriteString("\n")
+	}
+}
+
+// unions
+
+func printUnionDefinitions(unions []*graphql.Union, out *strings.Builder) {
+	sort.Slice(unions, func(i, j int) bool {
+		return unions[i].Name() < unions[j].Name()
+	})
+
+	for _, union := range unions {
+		printDescription(union.Description(), 0, out)
+		fmt.Fprintf(out, "union %s", union.Name())
+		printAppliedDirectives(union.AppliedDirectives, out)
+		typeNames := make([]string, 0, len(union.Types()))
+		for _, t := range union.Types() {
+			typeNames = append(typeNames, t.Name())
+		}
+		out.WriteString(" = ")
+		out.WriteString(strings.Join(typeNames, " | "))
+		out.WriteString("\n\n")
+	}
+}
+
+// scalars
+
+func printCustomScalars(scalars []*graphql.Scalar, out *strings.Builder) {
+	sort.Slice(scalars, func(i, j int) bool {
+		return scalars[i].Name() < scalars[j].Name()
+	})
+
+	for _, scalar := range scalars {
+		printDescription(scalar.Description(), 0, out)
+		fmt.Fprintf(out, "scalar %s", scalar.Name())
+		printAppliedDirectives(scalar.AppliedDirectives, out)
+		out.WriteString("\n\n")
+	}
+}
+
+// public API
+
+func PrintSchema(schema graphql.Schema) (string, error) {
+	enums := make([]*graphql.Enum, 0, 0)
+	inputObjects := make([]*graphql.InputObject, 0, 0)
+	interfaces := make([]*graphql.Interface, 0, 0)
+	objects := make([]*graphql.Object, 0, 0)
+	unions := make([]*graphql.Union, 0, 0)
+	scalars := make([]*graphql.Scalar, 0, 0)
+
+	buitlInScalars := map[string]bool{
+		"Boolean": true, "Float": true, "ID": true, "Int": true, "String": true,
+	}
+	for name, gqlType := range schema.TypeMap() {
+		// skip built in types
+		_, builtIn := buitlInScalars[name]
+		if strings.HasPrefix(name, "__") || builtIn {
+			continue
+		}
+
+		switch t := gqlType.(type) {
+		case *graphql.Enum:
+			enums = append(enums, t)
+		case *graphql.InputObject:
+			inputObjects = append(inputObjects, t)
+		case *graphql.Interface:
+			interfaces = append(interfaces, t)
+		case *graphql.Object:
+			objects = append(objects, t)
+		case *graphql.Union:
+			unions = append(unions, t)
+		case *graphql.Scalar:
+			scalars = append(scalars, t)
+		default:
+			// TODO unknown -> panic?
+		}
+	}
+
+	var sdl strings.Builder
+
+	printSchemaDefinition(schema, &sdl)
+	printDirectiveDefinitions(schema.Directives(), &sdl)
+	printEnumDefinitions(enums, &sdl)
+	printInputObjectDefinitions(inputObjects, &sdl)
+	printInterfaceDefinitions(interfaces, &sdl)
+	printObjectDefinitions(objects, &sdl)
+	printUnionDefinitions(unions, &sdl)
+	printCustomScalars(scalars, &sdl)
+
+	return sdl.String(), nil
+}
